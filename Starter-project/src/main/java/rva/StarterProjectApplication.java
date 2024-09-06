c47e9b836967575ac4f97eee41f11782d8fde3e7
Nikola <nikola.gajic@uns.ac.rs>
2024-09-06 20:06:35 +0200

    Backend

--- Starter-project/src/main/java/rva/StarterProjectApplication.java
+++ Starter-project/src/main/java/rva/StarterProjectApplication.java
@@ -1,7 +1,12 @@
 package rva;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 // GitHub Classroom Invite Link --> https://classroom.github.com/a/PgzNRc2E
 
@@ -11,5 +16,7 @@ public class StarterProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StarterProjectApplication.class, args); 
 	}
+	
+	
 
 }
